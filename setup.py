d208b92303b1338d350224f367e5469a95f2b82c
Lukas Mettler <lukas.mettler@student.kit.edu>
2025-04-04 12:02:53 +0200

    setup + license + dir structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lumiere',
+    version='0.1.0',
+    packages=find_packages(),
+    description='Python wrapper to streamline data fitting, error and uncertainty handling.',
+    author='Lukas E. Mettler',
+    url='https://https://github.com/LEMettler/lumiere',
+    license='MIT',
+)
